99717d28fd8974ec8520bbe32eca45b49983450b
Linh Le Kim <linhlekim@gmail.com>
2018-08-06 21:04:21 +0000

    added getReportContributors function

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
 console.log('Welcome to the github avatar downloader');
+
+function getRepoContributors(repoOwner, repoName, callback){
+
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result){
+  console.log("Errors:", err);
+  console.log("Result:" result);
+
+});
