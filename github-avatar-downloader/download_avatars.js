251530dc349f7e675b86c5d79cd5670317c8318e
Linh Le Kim <linhlekim@gmail.com>
2018-08-06 21:49:15 +0000

    changed gitignore and added authorization header

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -1,14 +1,24 @@
 var request = require('request');
+var secrets = require('secret');
 
 console.log('Welcome to the github avatar downloader');
 
 function getRepoContributors(repoOwner, repoName, callback){
-
+  var options = {
+    url : 'http://api.github.com/repos/' + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token 93e45a61126137a1875e87efa7fec945a0f4b08b'
+    }
+  };
+  request(options, function(err, res, body){
+    callback(err, body);
+  });
 
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors:", err);
-  console.log("Result:" result);
+  console.log("Result:", result);
 
 });
